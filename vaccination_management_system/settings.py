6769dcfe19373598fef8297f6e0f443e7e1e05cf
morsalin-islam335 <weshallovercome335@gmail.com>
2024-03-06 03:51:55 +0600

    some corrections

--- vaccination_management_system/settings.py
+++ vaccination_management_system/settings.py
@@ -3,7 +3,7 @@ import environ
 from pathlib import Path
 env = environ.Env()
 environ.Env.read_env()
-
+import dj_database_url
 
 from pathlib import Path
 
@@ -84,12 +84,22 @@ WSGI_APPLICATION = 'vaccination_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default=env("db"),
+        conn_max_age = 600
+    )
 }
+
 
 
 # Password validation
