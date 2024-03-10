d3ddd7404a4177e67abda16d1fc7b541fa914713
morsalin-islam335 <weshallovercome335@gmail.com>
2024-03-10 16:08:20 +0600

    service page design

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 
-from . views import home, about
+from . views import home, about, services
 urlpatterns = [
     path("", home, name = 'homepage'),
     path("about/",about, name = 'about'),
+    path("services/",services, name = 'services'),
 ]
--- core/views.py
+++ core/views.py
@@ -15,4 +15,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'service.html')
     
